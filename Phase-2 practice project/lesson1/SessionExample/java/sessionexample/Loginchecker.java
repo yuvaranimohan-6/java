afcf9442622cf2aa48ca7ead148f2938b10c49b9
Yuvarani <ms.yuvarani06@gmail.com>
2022-08-02 16:06:37 +0530

    phase 2

---
+++ Phase-2 practice project/lesson1/SessionExample/java/sessionexample/Loginchecker.java
@@ -0,0 +1,69 @@
+package sessionexample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Login checker
+ */
+@WebServlet("/Loginchecker")
+public class Loginchecker extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Loginchecker() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname=request.getParameter("txtUname");
+		String pword=request.getParameter("txtPassword");
+		PrintWriter out=response.getWriter();
+		if(uname.equalsIgnoreCase("yuvarani") && pword.equals("1234")){
+			out.println("You are Welcomed");
+		}
+		else
+			out.println("Invalid Username or Password");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		String uname=request.getParameter("txtUname");
+		String pword=request.getParameter("txtPassword");
+		PrintWriter out=response.getWriter();
+		//RequestDispatcher- an interface to include content on same page or different pages
+		RequestDispatcher rd;
+		if(uname.equalsIgnoreCase("yuvarani") && pword.equals("1234")){
+			//session object is created baby web container and generate session id
+			HttpSession session=request.getSession(true);//create a new object and store its reference in session object
+			session.setAttribute("username", uname);
+			
+			rd=request.getRequestDispatcher("Welcome");
+			rd.forward(request, response);
+		}
+		else {
+			out.println("Invalid Username or Password");
+			rd=request.getRequestDispatcher("Loginform.html");
+			rd.include(request, response);
+		}
+	}
+
+}
