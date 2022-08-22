d377820b5019d5699851cbaab8630f5c9258d793
Yuvarani <ms.yuvarani06@gmail.com>
2022-08-22 11:38:02 +0530

    phase3

---
+++ Phase-3 Practice Project/lesson2/ConsumeRestFulWebServivce/src/main/java/com/simplilearn/demo/ConsumeRestFulWebServivceApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeRestFulWebServivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeRestFulWebServivceApplication.class, args);
+	}
+
+}
