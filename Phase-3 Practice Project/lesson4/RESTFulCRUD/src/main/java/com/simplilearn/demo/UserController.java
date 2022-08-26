f531f42a7d2c63f17c71dea11cf99bbb0ffd9b2b
Yuvarani <ms.yuvarani06@gmail.com>
2022-08-26 17:40:54 +0530

    phase3

---
+++ Phase-3 Practice Project/lesson4/RESTFulCRUD/src/main/java/com/simplilearn/demo/UserController.java
@@ -0,0 +1,72 @@
+package com.simplilearn.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+	@Autowired
+	private UserService service;
+
+	/// create new record
+	@PostMapping("/")
+	public ResponseEntity<User> addUser(@RequestBody User u) {
+		User user = service.addUser(u);
+		if (user != null)
+			return new ResponseEntity<User>(user, HttpStatus.CREATED);
+		else
+			return new ResponseEntity<User>(user, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	// List of Users Or RETRIVE DATA
+	@GetMapping("/")
+	public List<User> getAllUser() {
+		return service.getAllUser();
+	}
+
+	// get user by id
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable int id) {
+		User user = service.getUserById(id);
+
+		if (user != null)
+			return new ResponseEntity<User>(user, HttpStatus.FOUND);
+		else
+			return new ResponseEntity<User>(user, HttpStatus.NOT_FOUND);
+	}
+
+	// update user by id
+	@PutMapping("/{id}")
+	public ResponseEntity<Object> updateUser(@RequestBody User user, @PathVariable int id) {
+		User data = service.updateUser(user, id);
+
+		if (data != null)
+			return new ResponseEntity<Object>(data, HttpStatus.OK);
+		else
+			return new ResponseEntity<Object>("User is Not Available", HttpStatus.NOT_FOUND);
+	}
+
+	/// DELETE USER BY ID
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deleteUser(@PathVariable int id) {
+
+		if (service.deleteUser(id))
+			return new ResponseEntity<Object>("User Deleted", HttpStatus.OK);
+		else
+			return new ResponseEntity<Object>("No User Found", HttpStatus.NOT_FOUND);
+	}
+
+}
---
+++ Phase-3 Practice Project/lesson4/RESTFulCRUD/src/main/java/com/simplilearn/demo/UserService.java
@@ -0,0 +1,61 @@
+package com.simplilearn.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepo repo;
+
+	// add method or CREATE RECORD
+	public User addUser(User u) {
+		return repo.save(u);
+	}
+
+	// List user Method
+	public List<User> getAllUser() {
+		return repo.findAll();
+	}
+
+	// get user by id
+	public User getUserById(int id) {
+		if (repo.findById(id).isPresent())
+			return repo.findById(id).get();
+		else
+			return null;
+
+	}
+
+	// update user by id
+	public User updateUser(User user, int id) {
+
+		if (repo.findById(id).isPresent()) {
+			User old = repo.findById(id).get();
+			old.setCountry(user.getCountry());
+			old.setName(user.getName());
+			old.setEmail(user.getEmail());
+
+			return repo.save(old);
+		} else {
+			return null;
+		}
+
+	}
+
+	/// DELETE USER BY ID
+	public boolean deleteUser(int id) {
+
+		if (repo.findById(id).isPresent()) {
+			repo.deleteById(id);
+			return true;
+		} else
+			return false;
+	}
+
+	
+
+}
